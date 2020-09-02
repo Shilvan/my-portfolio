71f0f417bcf8417dfa38832218d5ab37fb3c1860
shilvan <5.shilvandacosta@gmail.com>
2020-09-02 20:08:31 +0100

    add more detail to c# calculator project

--- static/js/portfolio.js
+++ static/js/portfolio.js
@@ -67,7 +67,7 @@ var main = function(){
 		$(".tech-used").append('<li>c#</li>');
 		$(".tech-used").append('<li>regex</li>');
 		
-		$("#more-details").text("More detailed info about everything, More detailed info about everything, More detailed info about everything, More detailed info about everything, More detailed info about everything, More detailed info about everything");
+		$("#more-details").text("Used Pluralsight courses to learn the basic concepts, best practices…" +\n +"This project instilled discipline in me, I used to learn c# before school at around 6 am.");
 
 		$("#demo-btn").css("display", "none");
 
